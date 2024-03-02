a5ccc1481aacd19187dcc8c63379b2c7956755e0
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-03-02 23:04:33 +0530

    middle of linked list

---
+++ src/LinkedList/MiddleOfLinkedList.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+
+public class MiddleOfLinkedList {
+    public static void main(String[] args) {
+        ListNode root = new ListNode(1);
+        root.next = new ListNode(2);
+        root.next.next = new ListNode(3);
+        // root.next.next.next = new ListNode(4);
+
+        ListNode slow = root, fast = root;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        System.out.println(slow.val);
+        return;
+    }
+}
