e19e842e54965df622bc9694a555e1b119a0b88d
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-03-19 23:04:40 +0530

    Delete node in BST

---
+++ src/Tree/DeleteNodeBST.java
@@ -0,0 +1,40 @@
+package Tree;
+
+public class DeleteNodeBST {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(10);
+        root.right = new TreeNode(15);
+        root.left.left = new TreeNode(20);
+        root.left.right = new TreeNode(30);
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            TreeNode minNode = findMin(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, root.val);
+        }
+        return root;
+    }
+
+    private TreeNode findMin(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+}
