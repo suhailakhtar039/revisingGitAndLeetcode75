bc96f5ee61054827dcc9233467160cab746a895d
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-04 18:11:11 +0530

    maximum nesting depth

---
+++ src/String/MaximumNestingDepth.java
@@ -0,0 +1,14 @@
+package String;
+
+public class MaximumNestingDepth {
+    public int maxDepth(String s) {
+        int res = 0, cur = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '(')
+                res = Math.max(res, ++cur);
+            if (s.charAt(i) == ')')
+                cur--;
+        }
+        return res;
+    }
+}
