7e8b9e7fb9613860c8429cd2c1f5722161708467
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-04 18:18:07 +0530

    contains duplicates 2

---
+++ src/Array/ContainsDuplicate2.java
@@ -0,0 +1,19 @@
+package Array;
+
+import java.util.HashMap;
+
+public class ContainsDuplicate2 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                int j = map.get(nums[i]);
+                if(Math.abs(i-j) <= k){
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
