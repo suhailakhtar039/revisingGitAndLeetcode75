34c9bd22ffa07f9baef04bd8a208a93e782d4a5a
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-26 23:19:58 +0530

    IncreasingTripletSubsequence modification

--- src/Array/IncreasingTripletSubsequence.java
+++ src/Array/IncreasingTripletSubsequence.java
@@ -2,20 +2,19 @@ package Array;
 
 public class IncreasingTripletSubsequence {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5};
+        int[] nums = {3, 2, 1, 4, 5};
 
         boolean ans = false;
         int small = Integer.MAX_VALUE, big = Integer.MAX_VALUE;
-        for(int i:nums){
-            if(i <= small){
+        for (int i : nums) {
+            if (i <= small) {
                 small = i;
-            }
-            else if(i <= big){
+            } else if (i <= big) {
                 big = i;
-            }
-            else{
+            } else {
                 ans = true;
             }
         }
+        System.out.println("small = " + small + " big = " + big + " ans = " + ans);
     }
 }
