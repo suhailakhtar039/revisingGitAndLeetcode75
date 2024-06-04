3365cabc1eca71433928d2ca0b6817142702f249
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-06-04 23:39:26 +0530

    second approach for DifferenceOfTwoArrays

--- src/Array/DifferenceOfTwoArrays.java
+++ src/Array/DifferenceOfTwoArrays.java
@@ -47,5 +47,27 @@ public class DifferenceOfTwoArrays {
         }
 
         System.out.println(res);
+
+        // List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        // List<Integer> ans1 = new ArrayList<Integer>();
+        // List<Integer> ans2 = new ArrayList<Integer>();
+        // Set<Integer> set1 = new HashSet<Integer>();
+        // Set<Integer> set2 = new HashSet<Integer>();
+        //
+        // for(int n : nums1) set1.add(n);
+        // for(int n : nums2) set2.add(n);
+        // for (int n : set1){
+        //     if(set2.contains(n) == false){
+        //         ans1.add(n);
+        //     }
+        // }
+        // for (int n : set2){
+        //     if(set1.contains(n) == false){
+        //         ans2.add(n);
+        //     }
+        // }
+        // ans.add(ans1);
+        // ans.add(ans2);
+        // return ans;
     }
 }
